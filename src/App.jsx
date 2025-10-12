0dd257141dc05a91a2e68da376100a25a2663bc8
David Gutierrez <dagd0918@gmail.com>
2025-10-12 11:57:46 -0300

    First commitment with some inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Contact Us</h1>
+      </header>
+      <main>
+        <form action="">
+          {/* The user's first name */}
+          <label htmlFor="first-name">First Name *</label>
+          <input id="first-name" type="text" />
+
+          {/* The user's last name */}
+          <label htmlFor="">Last Name *</label>
+          <input type="text" />
+
+          {/* The user's email address */}
+          <label htmlFor="">Email Address *</label>
+          <input type="email" />
+        </form>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
