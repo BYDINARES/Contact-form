06b893d72d9d2f3f18f4f0837b628af4523f8a5e
David <david.gutierrez.dinares@juanbosco.cl>
2025-10-15 16:53:53 -0300

    Adding some minor styling details to the inputs

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,52 @@ function App() {
       <main>
         <form action="">
           {/* The user's first name */}
-          <label htmlFor="first-name">First Name *</label>
+          <label className="label-user-infomation" htmlFor="first-name">
+            First Name *
+          </label>
           <input className="user-information" id="first-name" type="text" />
-
           {/* The user's last name */}
-          <label htmlFor="last-name">Last Name *</label>
+          <label className="label-user-infomation" htmlFor="last-name">
+            Last Name *
+          </label>
           <input className="user-information" id="last-name" type="text" />
-
           {/* The user's email address */}
-          <label htmlFor="email">Email Address *</label>
+          <label className="label-user-infomation" htmlFor="email">
+            Email Address *
+          </label>
           <input className="user-information" id="email" type="email" />
 
           <p>Query Type *</p>
-          <label>
+          <label className="query-type-option">
             <input type="radio" name="option" value="1" /> General Enquiry
           </label>
-          <label>
+          <label className="query-type-option">
             <input type="radio" name="option" value="2" /> Support Request
           </label>
 
-          <label htmlFor="message"> Message * </label>
-          <textarea name="mesage-box" id="message"></textarea>
+          <label className="label-message" htmlFor="message">
+            {" "}
+            Message *{" "}
+          </label>
+          <textarea
+            className="large-message"
+            name="mesage-box"
+            id="message"
+          ></textarea>
 
-          <label htmlFor="i-accept-button">
-            <input type="checkbox" name="users-approval" id="i-accept-button" />{" "}
+          <label
+            className="label-confirmation-button"
+            htmlFor="i-accept-button"
+          >
+            <input
+              className="confirmation-button"
+              type="checkbox"
+              name="users-approval"
+              id="i-accept-button"
+            />{" "}
             I consent to being contacted by the team *
           </label>
 
-          <label htmlFor=""></label>
           <input
             id="submit-button"
             type="submit"
