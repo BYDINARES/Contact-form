a363b9ea0dbc28f9c374d49ffcab24a0e7e66694
David <david.gutierrez.dinares@juanbosco.cl>
2025-10-17 15:47:55 -0300

    Adding the basic grid styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,38 +9,43 @@ function App() {
       <main>
         <form action="">
           {/* The user's first name */}
-          <label className="label-user-infomation" htmlFor="first-name">
+          <label className="label-user-infomation name" htmlFor="first-name">
             First Name *
+            <input className="user-information " id="first-name" type="text" />
           </label>
-          <input className="user-information" id="first-name" type="text" />
+
           {/* The user's last name */}
-          <label className="label-user-infomation" htmlFor="last-name">
+          <label
+            className="label-user-infomation last-name"
+            htmlFor="last-name"
+          >
             Last Name *
+            <input className="user-information" id="last-name" type="text" />
           </label>
-          <input className="user-information" id="last-name" type="text" />
+
           {/* The user's email address */}
-          <label className="label-user-infomation" htmlFor="email">
+          <label className="label-user-infomation email" htmlFor="email">
             Email Address *
+            <input className="user-information" id="email" type="email" />
           </label>
-          <input className="user-information" id="email" type="email" />
 
           <p className="pick-a-query-type">Query Type *</p>
-          <label className="query-type-option query-type-option--general">
+          <label className="query-type-option query-type-option--general general">
             <input type="radio" name="option" value="1" /> General Enquiry
           </label>
-          <label className="query-type-option query-type-option--support">
+          <label className="query-type-option query-type-option--support support">
             <input type="radio" name="option" value="2" /> Support Request
           </label>
 
           <label className="label-message" htmlFor="message">
             {" "}
             Message *{" "}
+            <textarea
+              className="large-message"
+              name="mesage-box"
+              id="message"
+            ></textarea>
           </label>
-          <textarea
-            className="large-message"
-            name="mesage-box"
-            id="message"
-          ></textarea>
 
           <label
             className="label-confirmation-button"
