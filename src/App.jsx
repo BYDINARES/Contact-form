db296c991f076373690103ce173e12b43540cd39
David <david.gutierrez.dinares@juanbosco.cl>
2025-10-22 19:10:21 -0300

    trying to figure out which structure I will give it to the check code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [numberOfErrors, setnumberOfErrors] = useState(0);
+
+  /*   function handleSubmit(event) {
+    event.preventDefault();
+    const formEl = event.currentTarget;
+    const formData = new FormData(formEl);
+    const email = formData.get("email");
+    const email
+    formEl.reset();
+  } */
   return (
     <>
       <header>
@@ -11,7 +22,12 @@ function App() {
           {/* The user's first name */}
           <label className="label-user-infomation name" htmlFor="first-name">
             First Name *
-            <input className="user-information " id="first-name" type="text" />
+            <input
+              className="user-information "
+              id="first-name"
+              type="text"
+              name="first-name"
+            />
           </label>
 
           {/* The user's last name */}
@@ -20,13 +36,23 @@ function App() {
             htmlFor="last-name"
           >
             Last Name *
-            <input className="user-information" id="last-name" type="text" />
+            <input
+              className="user-information"
+              id="last-name"
+              type="text"
+              name="last-name"
+            />
           </label>
 
           {/* The user's email address */}
           <label className="label-user-infomation email" htmlFor="email">
             Email Address *
-            <input className="user-information" id="email" type="email" />
+            <input
+              className="user-information"
+              id="email"
+              type="email"
+              name="email"
+            />
           </label>
 
           <p className="pick-a-query-type">Query Type *</p>
